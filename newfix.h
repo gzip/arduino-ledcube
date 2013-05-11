21699d3333175d47485c7be0faf970e79652c4f5
Gamaiel Zavala <ceriumz@hotmail.com>
2013-05-11 13:13:59 -0700

    Fix for Arduino 1.0.4

--- newfix.h
+++ newfix.h
@@ -1,10 +1,9 @@
+#if !defined(ARDUINO) || ARDUINO < 104
+
 #ifndef NEWFIX_H
 #define NEWFIX_H
 
 #include <stdlib.h>
-
-// not needed when this pull request is merged
-// https://github.com/arduino/Arduino/pull/73
 
 void* operator new[](size_t size)
 {
@@ -15,6 +14,8 @@ void operator delete[](void * ptr)
 {
     free(ptr);
 }
+
+#endif
 
 #endif
 
